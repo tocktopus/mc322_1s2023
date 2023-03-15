af128658ba0ea05b11fbb324b3b917d3b7e82b17
Sara Oliveira <s231288@dac.unicamp.br>
2023-03-14 21:50:20 -0300

    Commit inicial, cria arquivos das classes

---
+++ mc322_1s2023/src/mc322_1s2023/Main.java
@@ -0,0 +1,10 @@
+package mc322_1s2023;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ mc322_1s2023/src/mc322_1s2023/Veiculo.java
@@ -0,0 +1,5 @@
+package mc322_1s2023;
+
+public class Veiculo {
+
+}
