1051c89c94a5c617fc10162e9ee42082d449ca29
Sara Oliveira <s231288@dac.unicamp.br>
2023-03-14 22:14:47 -0300

    Cria atributos e getters e setters das classes Seguradora e Veiculo

--- mc322_1s2023/src/mc322_1s2023/Veiculo.java
+++ mc322_1s2023/src/mc322_1s2023/Veiculo.java
@@ -1,5 +1,41 @@
 package mc322_1s2023;
 
 public class Veiculo {
+	private String placa;
+	private String marca;
+	private String modelo;
+	
+	//construtor
+	public Veiculo(String placa, String marca, String modelo){
+		this.placa = placa;
+		this.marca = marca;
+		this.modelo = modelo;
+	}
+	
+	//getters e setters
+	public String getPlaca() {
+		return placa;
+	}
 
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	
 }
