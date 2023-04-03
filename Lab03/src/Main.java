409b3828f8e2d894e8d1c0ffb3ef44ef8d0d4ec7
Sara <sara.bsoliveira@gmail.com>
2023-04-03 15:13:08 -0300

    Cria alguns dos métodos da seguradora

--- Lab03/src/Main.java
+++ Lab03/src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args){
         //TO-DO: deixar main nos conformes das instrucoes, por enquanto estamos usando apenas para testes rapidos
 
-        ClientePJ c = new ClientePJ("A", "B", null, "A", "F", "C", "123", null);
+        ClientePJ c = new ClientePJ("A", "B", null, "A", "F", "C", "12.445.351/0001-95", null);
+        ClientePJ c2 = new ClientePJ("oi", "B", null, "A", "F", "C", "12.445.351/0001-95", null);
+        ClientePF k = new ClientePF("A2", "B", null, "A", "F", "C", "132.104.950-17", null);
+
         if(c.validarCNPJ("19733821000165")){
             System.out.println("valido");
         }else{
@@ -21,8 +24,15 @@ public class Main {
         
         //System.out.println(c);
 
-        Sinistro s = new Sinistro("30/06/2020", null, null, v3, c);
+        //Sinistro s = new Sinistro("30/06/2020", null, null, v3, c);
 
-        System.out.println(s);
+        //System.out.println(s);
+
+        Seguradora seg = new Seguradora();
+        System.out.println(seg.cadastrarCliente(c));
+        System.out.println(seg.cadastrarCliente(c2));
+        System.out.println(seg.cadastrarCliente(k));
+        System.out.println(seg.removerCliente(c2));
+        System.out.println(seg.listarClientes("pj"));
     }
 }
--- Lab03/src/Seguradora.java
+++ Lab03/src/Seguradora.java
@@ -4,6 +4,8 @@
  * Ultima modificacao: 03/04/2023
  * 
  * Material usado na disciplina MC322
+ * 
+ * TO-DO: metodos gerarSinistro, visualizarSinistro e listarSinistro
  */
 import java.util.ArrayList;
 
@@ -17,6 +19,8 @@ public class Seguradora {
 
 	//construtores
     public Seguradora(){
+        listaSinistros = new ArrayList<Sinistro>();
+        listaClientes = new ArrayList<Cliente>();
     }
 
 	public Seguradora (String nome, String telefone, String email, String endereco, ArrayList<Sinistro> listaSinistros, ArrayList<Cliente> listaClientes) {
@@ -28,8 +32,72 @@ public class Seguradora {
         listaClientes = new ArrayList<Cliente>();
 	}
 
-    //TO-DO: metodos da seguradora
-    
+    //metodos relacionados ao atributo listaClientes:
+    public boolean cadastrarCliente(ClientePF cliente){
+        /* Insere um cliente tipo pessoa física na listaClientes.
+		 * Entrada: ClientePF cliente (cliente a ser cadastrado)
+		 * Saida: valor booleano (true se o cadastro for realizado com sucesso, false se nao for) */
+
+        if(cliente.validarCPF(cliente.getCpf()) && !listaClientes.contains(cliente)){ //verifica se o cpf é valido e se o cliente ja foi cadastrado
+            listaClientes.add(cliente);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean cadastrarCliente(ClientePJ cliente){
+        /* Insere um cliente tipo pessoa jurídica na listaClientes.
+		 * Entrada: ClientePJ cliente (cliente a ser cadastrado)
+		 * Saida: valor booleano (true se o cadastro for realizado com sucesso, false se nao for) */
+
+        if(cliente.validarCNPJ(cliente.getCnpj()) && !listaClientes.contains(cliente)){ //verifica se o cnpj é valido e se o cliente ja foi cadastrado
+            listaClientes.add(cliente);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean removerCliente(Cliente cliente){
+        /* Remove um cliente da listaClientes.
+		 * Entrada: Cliente cliente (cliente a ser removido)
+		 * Saida: valor booleano (true se o cliente for encontrado na lista, false se nao for) */
+
+        for(Cliente c : listaClientes){
+            if(c.equals(cliente)){
+                listaClientes.remove(c);
+                return true;
+            }  
+        }
+        return false;
+        
+    }
+
+    public ArrayList<Cliente> listarClientes(String tipoCliente){
+        /* Separa todos os clientes de um determinado tipo em uma lista
+		 * Entrada: String tipoCliente (String informando o tipo de cliente que se deseja listar)
+		 * Saida: ArrayList<Cliente> lista (Lista contendo todos os clientes correspondentes) */
+
+        ArrayList<Cliente> lista = new ArrayList<Cliente>();
+        if(tipoCliente.equals("pf")){
+            for(Cliente c : listaClientes){
+                if(c instanceof ClientePF){ //verifica se o cliente na posicao atual eh do tipo pessoa fisica
+                    lista.add(c);
+                }
+            }
+        }
+        if(tipoCliente.equals("pj")){
+            for(Cliente c : listaClientes){
+                if(c instanceof ClientePJ){ //verifica se o cliente na posicao atual eh do tipo pessoa juridica
+                    lista.add(c);
+                }
+            }
+        }
+        return lista;
+    }
+
+
 	//getters e setters
 	public String getNome() {
 		return nome;
